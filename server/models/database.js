079088f22554127903f78a28b72471f83140da03
Ndukwe Daniel <danielndukwe11@gmail.com>
2022-12-28 12:05:20 -0800

    First commit

--- server/models/database.js
+++ server/models/database.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, 
+mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, 
     useUnifiedTopology: true });
 
 const db = mongoose.connection;
